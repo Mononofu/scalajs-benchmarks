7a5b35158f3a5a2652733ba9a2905fb2851ac2af
Julian Schrittwieser <j.schrittwieser@gmail.com>
2015-02-07 18:54:39 +0000

    add fake output stream

--- scalajs_preamble.sh
+++ scalajs_preamble.sh
@@ -14,4 +14,55 @@ object ${NAME}_js extends js.JSApp {
     ${NAME}.main(process.argv.drop(2).toArray)
   }
 }
+
+package java {
+package io {
+
+// hacked output stream, only writes to stdout
+class BufferedOutputStream(out: Any) {
+  var buf: Array[Byte] = new Array[Byte](8096)
+  var count: Int = 0
+
+  def close(): Unit = {
+    flush()
+    println()
+  }
+
+  def flush(): Unit = {
+    if (count > 0) {
+      print(new String(buf.take(count).map(_.toChar)))
+      count = 0
+    }
+  }
+
+  def write(b: Array[Byte]): Unit = {
+    if (b.length > buf.length - count) {
+      flush()
+      print(new String(b.map(_.toChar)))
+    } else {
+      buf = buf.take(count) ++ b
+      count += b.length
+      if (count == buf.length) {
+        flush()
+      }
+    }
+  }
+
+  def write(b: Int): Unit = {
+    if (count >= buf.length) {
+      print(buf.take(count).map(_.toChar).toString + b.toChar.toString)
+      count = 0
+    } else {
+      buf(count) = b.toByte
+      count += 1
+      if (count >= buf.length) {
+        flush()
+      }
+    }
+  }
+}
+
+
+} // package io
+} // package java
 "
